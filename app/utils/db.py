94277e7f6b402e05ffcb7cd5ce75d2a4c0e8f24e
Residential Kiwi <113327928+ResidentKiwi@users.noreply.github.com>
2025-06-17 13:15:44 -0300

    Create db.py

---
+++ app/utils/db.py
@@ -0,0 +1,7 @@
+from supabase import create_client, Client
+import os
+
+SUPABASE_URL = os.getenv("SUPABASE_URL")
+SUPABASE_SERVICE_ROLE = os.getenv("SUPABASE_SERVICE_ROLE")
+
+supabase: Client = create_client(SUPABASE_URL, SUPABASE_SERVICE_ROLE)
