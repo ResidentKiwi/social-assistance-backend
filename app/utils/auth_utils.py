0824c4f75f9890f7cc626830071ede9dee5996ba
Residential Kiwi <113327928+ResidentKiwi@users.noreply.github.com>
2025-06-23 18:48:51 -0300

    Update auth_utils.py

--- app/utils/auth_utils.py
+++ app/utils/auth_utils.py
@@ -1,6 +1,6 @@
 from fastapi import Request, HTTPException, status, Depends
 from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
-import jwt
+from jose import JWTError, jwt
 import os
 
 from app.utils.db import get_social_db
@@ -19,14 +19,14 @@ class JWTBearer(HTTPBearer):
             return payload
         except jwt.ExpiredSignatureError:
             raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Token expirado")
-        except jwt.InvalidTokenError:
+        except JWTError:
             raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Token inválido")
 
 async def admin_required(request: Request, db=Depends(get_social_db)):
     uid = request.state.user.get("sub")
     if not uid:
         raise HTTPException(status_code=400, detail="ID de usuário não encontrado no token.")
-    
+
     user = db.query(Users).filter_by(id=uid).first()
     if not user or not user.is_admin:
         raise HTTPException(status_code=403, detail="Acesso restrito a administradores.")
